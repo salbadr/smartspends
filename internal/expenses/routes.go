b155cd38082b17e5be16dff5eb5217ba97aee1ce
Salman Badr <mailsbadr@gmail.com>
2026-08-02 12:27:12 -0400

    2-created server and structure

---
+++ internal/expenses/routes.go
@@ -0,0 +1,30 @@
+package expenses
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func expensesHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		var er ExpenseRequest
+
+		if err := json.NewDecoder(req.Body).Decode(&er); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		defer req.Body.Close()
+
+		w.Header().Set("Content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		payload := GetExpenses()
+		if err := json.NewEncoder(w).Encode(payload); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
+
+func RegisterExpenseRoutes(router *http.ServeMux) {
+	router.Handle("POST /api/v1/expenses", expensesHandler())
+}
